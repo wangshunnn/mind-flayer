79ca216c2ddf677069fc3828ed89d87042cce079
Soon Wang <wangshunnn@gmail.com>
2025-12-20 16:52:45 +0800

    chore: format vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -21,12 +21,12 @@ export default defineConfig(async () => ({
       ? {
           protocol: "ws",
           host,
-          port: 1421,
+          port: 1421
         }
       : undefined,
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-  },
+      ignored: ["**/src-tauri/**"]
+    }
+  }
 }))
